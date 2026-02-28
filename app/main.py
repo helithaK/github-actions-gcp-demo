89e363fe9818b0bd0ceeb0b2c4d054b39e08adc8
helithak <helitha56@gmail.com>
2026-03-01 02:49:54 +0530

    Minor change

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello from GitHub Actions CI/CD!"}
+    return {"message": "Hello from GitHub Actions CI/CD! - created by hexxa"}
